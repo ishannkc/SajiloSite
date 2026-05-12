88f766141fa3b1df6ac901373e0334afe73782ba
anujmagar777 <magaranuj55@gmail.com>
2026-05-12 11:31:33 +0545

    auth routes created with middleware included

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDb from './config/db.js'
-
 dotenv.config()
+import connectDb from './config/db.js'
+import authRouter from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const port = process.env.PORT || 5000
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(cors({
+    origin: 'http:localhost:5173',
+    credentials: true
+}))
+
+app.use('/api/auth', authRouter)
 
 app.listen(port,()=>{
     console.log('Server started')
