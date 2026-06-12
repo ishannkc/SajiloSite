041ae0634c5730556f53178de00abdcf83785347
ishannkc <ishankc777@gmail.com>
2026-06-12 21:06:21 +0545

    feat(router): add openRouter API integration and response generation function

---
+++ server/config/openRouter.js
@@ -0,0 +1,33 @@
+const openRouter="https://openrouter.ai/api/v1/chat/completions"
+const model="deepseek/deepseek-chat"
+
+export const generateResponse=async (prompt)=>{
+const res =await fetch(openRouter, {
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    model: model,
+    messages: [
+        {role:"system",
+          content:"You must return only valid raw JSON."},
+      {
+        role: 'user',
+        content: prompt,
+      },  
+    ],
+    temperature:0.2
+  }),
+});
+
+if(!res.ok){
+    const err=await res.text()
+    throw new Error("openRouter err"+err)
+}
+const data=await res.json() 
+return data
+
+}
+
