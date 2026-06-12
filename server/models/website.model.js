32ba0557ac4795b136001663e3a8bf006d96ad52
ishannkc <ishankc777@gmail.com>
2026-06-12 21:51:16 +0545

    feat(model): define website and message schemas for conversation history and website management

---
+++ server/models/website.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+//conversation history
+const messageSchema = new mongoose.Schema({
+    role: {
+        type:String,
+        enum: ['ai', 'user'],
+        required: true
+    },
+    content: {
+        type:String,
+        required: true
+    }
+},{timestamps: true} )
+
+const websiteSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: {
+        type: String,
+        default: "Untitled Website"
+    },
+    latestCode: {
+        type:String,
+        required: true
+    },
+    conversation:[
+        messageSchema
+    ],
+    deployed: {
+        type:Boolean,
+        default: false
+    },
+    deployedUrl: {
+        type: String,
+    },
+    //uniquely identify generated websites(each generated website has a unique id attached to it)
+    slug: {
+        type:String,
+        unique: true
+    }
+},{timestamps: true})
+
+const Website = mongoose.model('Website', websiteSchema)
+export default Website
