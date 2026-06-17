f83ad6af23d4fb6b7e9363af6f807bccfe58b0f6
ishannkc <ishankc777@gmail.com>
2026-06-17 08:28:16 +0545

    feat(Editor): implement chat functionality for user prompts and AI responses feat(routes): add changes endpoint for updating website content based on user input

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -9,7 +9,24 @@ function Editor() {
 
   const [website, setWebsite] = useState(null);
   const [error, setError] = useState("");
+  const [code, setCode] = useState('')
+  const [messages, setMessages] = useState([])
+  const [prompt, setPrompt]=useState('')
   const iframeRef = useRef(null);
+
+
+  const handleUpdate = async () => {
+    setMessages((m)=>[...m,{role:'user', content:prompt}])
+      try {
+          const result = await axios.post(`${serverUrl}/api/website/update/${id}`, {prompt},
+            {withCredentials: true})
+            console.log(result)
+            setMessages((m)=>[...m,{role:'ai', content:result.data.message}])
+            setCode(result.data.code)
+      } catch (error) {
+          console.log(error)
+      }
+  }
 
   useEffect(() => {
     const handleGetWebsite = async () => {
@@ -20,6 +37,10 @@ function Editor() {
         );
         console.log(result);
         setWebsite(result.data);
+
+        setCode(result.data.latestCode)
+        setMessages(result.data.conversation)
+
       } catch (error) {
         console.log(error);
         setError(error?.response?.data?.message || "Failed to load website");
@@ -29,8 +50,8 @@ function Editor() {
   }, [id]);
 
   useEffect(() => {
-    if (!iframeRef.current || !website?.latestCode) return;
-    const blob = new Blob([website?.latestCode], { type: "text/html" });
+    if (!iframeRef.current || !code) return;
+    const blob = new Blob([code], { type: "text/html" });
     const url = URL.createObjectURL(blob);
     iframeRef.current.src = url;
     return () => URL.revokeObjectURL(url);
@@ -56,42 +77,11 @@ function Editor() {
     <div className="h-screen w-screen flex bg-black text-white overflow-hidden">
       <aside className="hidden lg:flex w-[380px] flex-col border-r border-white/10 bg-black/80">
         <Header />
-        <Chat />
-      </aside>
 
-      <div className="flex-1 flex flex-col">
-        <div className="h-14 px-4 flex justify-between items-center border-b border-white/10 bg-black/80">
-          <span className="text-xs text-zinc-400">Live Preview </span>
-          <div className="flex gap-2">
-            <button className="flex items-center gap-2 px-4 py-1.5 rounded-lg bg-linear-to-r from-indigo-500 to-purple-500 text-sm font-semibold hover:scale-105 transition">
-              Deploy
-            </button>
-            <button className="p-2">
-              <Code2 size={18} />
-            </button>
-            <button className="p-2">
-              <Monitor size={18} />
-            </button>
-          </div>
-        </div>
-
-        <iframe ref={iframeRef} className="flex-1 w-full bg-white"></iframe>
-      </div>
-    </div>
-  );
-  function Header() {
-    return (
-      <div className="h-14 px-4 flex items-center justify-between border-b border-white/10">
-        <span className="font-semibold truncate">{website.title}</span>
-      </div>
-    );
-  }
-
-  function Chat() {
-    return (
-      <>
+        {/* chat */}
+          <>
         <div className="flex-1 overflow-y-auto px-4 py-4 space-y-4">
-          {website.conversation?.map((m, i) => (
+          {messages?.map((m, i) => (
             <div
               key={i}
               className={`max-w-[85%] ${
@@ -116,19 +106,50 @@ function Editor() {
 
          <div className="p-3 border-t border-white/10">
             <div className="flex gap-2">
-              <textarea
-                rows="1"
+              <input
                 placeholder="Describe Changes..."
                 className="flex-1 resize-none rounded-2xl px-4 py-3 bg-white/5 border border-white/10 text-sm outline-none"
-              ></textarea>
-              <button className="px-4 py-3 rounded-2xl bg-white text-black">
+                onChange={(e)=>setPrompt(e.target.value)} value={prompt}/>
+              <button className="px-4 py-3 rounded-2xl bg-white text-black"
+                onClick={handleUpdate}
+              >
                 <Send size={14}/>
               </button>
             </div>
           </div>
       </>
+      </aside>
+
+      <div className="flex-1 flex flex-col">
+        <div className="h-14 px-4 flex justify-between items-center border-b border-white/10 bg-black/80">
+          <span className="text-xs text-zinc-400">Live Preview </span>
+          <div className="flex gap-2">
+            <button className="flex items-center gap-2 px-4 py-1.5 rounded-lg bg-linear-to-r from-indigo-500 to-purple-500 text-sm font-semibold hover:scale-105 transition">
+              Deploy
+            </button>
+            <button className="p-2">
+              <Code2 size={18} />
+            </button>
+            <button className="p-2">
+              <Monitor size={18} />
+            </button>
+          </div>
+        </div>
+
+        <iframe ref={iframeRef} className="flex-1 w-full bg-white"></iframe>
+      </div>
+    </div>
+  );
+
+
+  function Header() {
+    return (
+      <div className="h-14 px-4 flex items-center justify-between border-b border-white/10">
+        <span className="font-semibold truncate">{website.title}</span>
+      </div>
     );
   }
 }
+ 
 
 export default Editor;
